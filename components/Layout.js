7ac1ff3747fcc7dee5a4f5afeeadce4c865aa111
Thinh Dang <134244643+ThinhDN02@users.noreply.github.com>
2024-07-29 20:19:31 +0700

    update layout

--- components/Layout.js
+++ components/Layout.js
@@ -1,62 +1,54 @@
 import React from 'react';
+import InstagramIcon from './InstagramIcon';
 import { Layout, Menu, Input } from 'antd';
-import { HomeOutlined, SearchOutlined, CompassOutlined, HeartOutlined, UserOutlined } from '@ant-design/icons';
+import { HomeOutlined, CompassOutlined, HeartOutlined, UserOutlined } from '@ant-design/icons';
 import Link from 'next/link';
 
 const { Header, Content, Sider } = Layout;
 const { Search } = Input;
 
 const AppLayout = ({ children }) => {
-  return (
-    <Layout>
-      <Header style={{ display: 'flex', alignItems: 'center', justifyContent: 'space-between', background: '#fff', padding: '0 50px' }}>
-        <div className="logo">
-          <Link href="/">Instagram</Link>
-        </div>
-        <Search
-          placeholder="Search"
-          enterButton={<SearchOutlined />}
-          style={{ maxWidth: '300px' }}
-        />
-        <Menu mode="horizontal" style={{ flex: 1, justifyContent: 'flex-end', background: 'none', borderBottom: 'none' }}>
-          <Menu.Item key="home" icon={<HomeOutlined />} />
-          <Menu.Item key="explore" icon={<CompassOutlined />} />
-          <Menu.Item key="notifications" icon={<HeartOutlined />} />
-          <Menu.Item key="profile" icon={<UserOutlined />} />
-        </Menu>
-      </Header>
-      <Layout>
-        <Sider width={80} className="site-layout-background">
-          <Menu mode="vertical" defaultSelectedKeys={['1']} style={{ height: '100%', borderRight: 0 }}>
-            <Menu.Item key="1" icon={<HomeOutlined />}>
-              Home
-            </Menu.Item>
-            <Menu.Item key="2" icon={<CompassOutlined />}>
-              Explore
-            </Menu.Item>
-            <Menu.Item key="3" icon={<HeartOutlined />}>
-              Notifications
-            </Menu.Item>
-            <Menu.Item key="4" icon={<UserOutlined />}>
-              Profile
-            </Menu.Item>
-          </Menu>
-        </Sider>
-        <Layout style={{ padding: '24px' }}>
-          <Content
-            style={{
-              padding: 24,
-              margin: 0,
-              minHeight: 280,
-              background: '#fff',
-            }}
-          >
-            {children}
-          </Content>
+    return (
+        <Layout style={{ minHeight: '100vh' }}>
+            <Layout style={{ flex: 1 }}>
+                <Sider width={200} className="site-layout-background">
+                    <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', height: '64px' }}>
+                        <Link href="/" legacyBehavior>
+                            <a>
+                                <InstagramIcon />
+                            </a>
+                        </Link>
+                    </div>
+                    <Menu mode="vertical" defaultSelectedKeys={['1']} style={{ height: 'calc(100% - 64px)', borderRight: 0 }}>
+                        <Menu.Item key="1" icon={<HomeOutlined />}>
+                            Home
+                        </Menu.Item>
+                        <Menu.Item key="2" icon={<CompassOutlined />}>
+                            Explore
+                        </Menu.Item>
+                        <Menu.Item key="3" icon={<HeartOutlined />}>
+                            Notifications
+                        </Menu.Item>
+                        <Menu.Item key="4" icon={<UserOutlined />}>
+                            Profile
+                        </Menu.Item>
+                    </Menu>
+                </Sider>
+                <Layout style={{ padding: '24px' }}>
+                    <Content
+                        style={{
+                            padding: 24,
+                            margin: 0,
+                            minHeight: 280,
+                            background: '#fff',
+                        }}
+                    >
+                        {children}
+                    </Content>
+                </Layout>
+            </Layout>
         </Layout>
-      </Layout>
-    </Layout>
-  );
+    );
 };
 
 export default AppLayout;
