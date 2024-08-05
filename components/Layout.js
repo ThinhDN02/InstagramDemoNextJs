8740f23c16dbe9a0b828b1c8594306198406d369
Thinh Dang <134244643+ThinhDN02@users.noreply.github.com>
2024-08-05 17:35:56 +0700

    no message

--- components/Layout.js
+++ components/Layout.js
@@ -57,12 +57,12 @@ const AppLayout = ({ children }) => {
                         }}
                         theme="light"
                     >
-                        <h2>hiện tại hình ảnh đang load hên xui</h2>
                         <Link href="/" legacyBehavior>
                             <a style={{ display: 'flex', alignItems: 'center', height: '120px', paddingLeft: 20 }}>
                                 <InstagramIcon style={{ color: '#000', width: '120px', height: 'auto' }} />
                             </a>
                         </Link>
+                        <h2>hiện tại hình ảnh đang load hên xui</h2>
                         <Menu.Item key="1" icon={<HomeOutlined style={{ fontSize: '24px' }} />}>
                             <Link href="/">Trang chủ</Link>
                         </Menu.Item>
